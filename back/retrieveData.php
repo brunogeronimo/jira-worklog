dd76b55b5fc607ff402493943b01ec84815de5f2
Bruno Geronimo <hi@bruno.works>
2017-09-14 11:54:32 -0300

    This app has been refactored to be object-oriented

--- back/retrieveData.php
+++ back/retrieveData.php
@@ -1,101 +1,45 @@
 <?php
-	require __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__.'/JiraInterface.php';
+	require_once __DIR__.'/Filter.php';
 	define('JIRA_DEFAULT_API_URL', '/rest/api/2');
+	define('BR_DATE_REGEXP', '/^([0-9]{2}\/){2}\/[0-9]{4}$/');
 	header("Content-Type: application/json");
 
 	$requestContent = file_get_contents('php://input');
 	$requestContent = json_decode($requestContent);
 
-	$username = $requestContent->username;
-	$password = $requestContent->password;
-	$url = $requestContent->url;
 	$jql = $requestContent->jql;
-
-	$jiras = array();
-	$timePerUser = array();
-	$timePerUser['total'] = 0;
-	$timePerUser['averageTimePerUser'] = 0;
-	$timePerUser['averageTimePerJira'] = 0;
-	$timePerUser['users'] = array();
-
-
-
-	$client = new GuzzleHttp\Client();
-	try{
-		$res = $client->get(
-				($url.JIRA_DEFAULT_API_URL.'/search'),
-				[
-					'query' => [
-						'jql' => $jql,
-						'startAt' => 0,
-						'maxResults' => 3500
-					],
-					'auth' => [$username, $password],
-					'debug' => false
-					
-				]
-			);
-	}catch(\GuzzleHttp\Exception\ClientException $e){
-		die(json_encode(array("status" => "ERR_USERNAME_PASSWORD")));
-	}
+	$from = "";
+	$to = "";
+	$worklogUsername = "";
 	
-	$content = json_decode($res->getBody());
-	$jiraLinks = array();
-	foreach ($content->issues as $issue) {
-		$jiraLinks[] = $issue->self;
+	if (isset($requestContent->from)){
+		$from = $requestContent->from;
+	}
+	if (isset($requestContent->to)){
+		$to = $requestContent->to;
+	}	
+	if (isset($requestContent->worklogUsername)){
+		$worklogUsername = $requestContent->worklogUsername;
 	}
 
-
-
+	$jiraInterface = JiraInterface::create()
+						->setUsername($requestContent->username)
+						->setPassword($requestContent->password)
+						->setUrl($requestContent->url);
 	
-	foreach ($jiraLinks as $link) {
-		$resIssue = json_decode($client->get($link,
-					[
-						'auth' => [$username, $password]
-					]
-				)->getBody());
-		$resIssueAux = $resIssue->fields;
-		if ($resIssueAux->worklog->total <> 0){
-			$auxJira = array();
-			$auxJira['key'] = $resIssue->key;
-			$auxJira['summary'] = $resIssueAux->summary;
-			$auxJira['priority'] = $resIssueAux->priority->name;
-			$auxJira['type'] = $resIssueAux->issuetype->name;
-			$auxJira['status'] = $resIssueAux->status->name;
-			$sprintAux = array();
-			$max = count($resIssueAux->customfield_10991) - 1;
-			preg_match('/(name=)([a-zA-Z0-9 ]*)/', $resIssueAux->customfield_10991[$max], $sprintAux);
-			$auxJira['sprint'] = $sprintAux[2];
-			$auxJira['worklogs'] = array();
-			foreach ($resIssueAux->worklog->worklogs as $worklog) {
-				$created = new DateTime($worklog->started);
-				$auxJira['worklogs'][] = array(
-						"author" => ($worklog->author->displayName . " [{$worklog->author->name}]"),
-						"created" => $created->format("d/m/Y"),
-						"timeSpent" => $worklog->timeSpent,
-						"timeSpentSeconds" => $worklog->timeSpentSeconds
-					);
-				if (!isset($timePerUser['users'][$worklog->author->name])){
-					$timePerUser['users'][$worklog->author->name] = 0;
-				}
-				$timePerUser['users'][$worklog->author->name] += $worklog->timeSpentSeconds;
-				$timePerUser['total'] += $worklog->timeSpentSeconds;
-			}
-			$jiras[] = $auxJira;
-		}
-	}
-	$jiras['size'] = count($jiras);
+	$jiraFilter = Filter::create()
+					->setFrom($from)
+					->setTo($to)
+					->setWorklogUsername($worklogUsername);
 
-	$totalJiras = count($jiras);
-	$totalUsers = count($timePerUser['users']);
-	if ($timePerUser['total'] > 0){
-		$timePerUser['averageTimePerUser'] = $timePerUser['total'] / $totalUsers;
-		$timePerUser['averageTimePerJira'] = $timePerUser['total'] / $totalJiras;
-	}
+	$queryResult = $jiraInterface->runQuery($jql)->filter($jiraFilter);
 
 	$body = array();
-	$body['jiras'] = $jiras;
-	$body['timePerUser'] = $timePerUser;
+	$body['jiras'] = $queryResult->getResultList();
+	$body['jiras']['size'] = $queryResult->count();
+	$body['statistics'] = $queryResult->totalize();
 
 	echo (json_encode($body));
 ?>
